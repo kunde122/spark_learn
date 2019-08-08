5a43c2cefd1497e22a012820c9b34baa9759a0d4
曹振天 <caozhentian@yadingdata.com>
2019-08-08 09:39:52 +0800

    1

--- my_spark.py
+++ my_spark.py
@@ -1,4 +1,9 @@
 from pyspark import SparkContext
 
 sc=SparkContext(appName='test')
-lines = sc.textFile("E:\pythonProject\pysparktest.txt")
+lines = sc.textFile("./data/total00.txt")
+
+gre=lines.count()
+fsd=lines.first()
+print(fsd.encode('utf-8'))
+print('done')
